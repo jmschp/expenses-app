a44cf1e0503e1e1759107d9473cf75a10b3e24a3
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-09-24 18:41:17 +0100

    feat add expense filter by year

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,14 +1,24 @@
+import { useState } from 'react'
 import Card from "../wrapper/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
-import './Expenses.css'
+import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, SetYear] = useState("2019");
+
+  const filterExpenseHandler = (selectedYear) => {
+    SetYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter filteredYear={filteredYear} onFilterExpense={filterExpenseHandler} />
       {props.expenses.map((expense) => {
         return (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
